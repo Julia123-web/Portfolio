2e61f27643e7fd89a881ffe13cdaee8eb59f6c2f
Julia <juliadesouzaesilva@gmail.com>
2024-03-23 13:56:00 +0100

    target blank included

--- src/app/components/Footer/footer.js
+++ src/app/components/Footer/footer.js
@@ -9,13 +9,18 @@ const Footer = () => {
 		<div id="contact" className="relative top-80 flex flex-col items-center">
 			<div className="text-white">&copy; 2024 Julia Silva. All Rights Reserved.</div>
 			<br />
-			<div className="flex space-x-4 scale-100">
-				<a href="https://github.com" className="text-white hover:text-white">
+			<div className="flex space-x-4 scale-150 relative top-1">
+				<a
+					href="https://github.com/Julia123-web"
+					target="_blank"
+					className="text-white hover:text-white"
+				>
 					<FaGithub></FaGithub>
 				</a>
 
 				<a
 					href="https://www.linkedin.com/in/julia-silva-2b242b171/"
+					target="_blank"
 					className="text-white hover:text-white"
 				>
 					<CiLinkedin></CiLinkedin>
@@ -23,9 +28,15 @@ const Footer = () => {
 			</div>
 			<br />
 			<div className="text-white mt-2">
-				<span>Email: juliadesouzaesilva@gmail.com</span>
-				<span className="ml-4">Phone: +310615125932</span>
+				<span>
+					Email:{' '}
+					<a href="mailto:juliadesouzaesilva@gmail.com">juliadesouzaesilva@gmail.com</a>
+				</span>
+				<span className="ml-4">
+					Phone: <a href="tel:+310615125932">+310615125932</a>
+				</span>
 			</div>
+
 			<br />
 		</div>
 	);
