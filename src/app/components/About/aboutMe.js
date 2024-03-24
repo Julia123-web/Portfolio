af89fea59f4bfbe57f2f3310415ef265a77f3c6d
Julia <juliadesouzaesilva@gmail.com>
2024-03-24 22:25:34 +0100

    font-size portrait

--- src/app/components/About/aboutMe.js
+++ src/app/components/About/aboutMe.js
@@ -8,7 +8,7 @@ export default function About() {
 		<div id="about">
 			<div className="container mx-auto flex flex-col items-center px-5 py-10 md:flex-row md:items-center">
 				<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-					<div className="font-mono sm:text-4xl text-3xl mb-4 font-medium from-slate-200">
+					<div className="font-mono text-4xl mb-4 font-medium">
 						I am a Frontend Developer
 					</div>
 					<div className="mb-8 leading-relaxed">
--- src/app/components/Hero/hero.js
+++ src/app/components/Hero/hero.js
@@ -6,7 +6,9 @@ import BackgroundAnimation from '../backgroundAnimation/backgroundAnimation';
 export default function Hero() {
 	return (
 		<div className="flex justify-center items-center text-center text-4xl" id="hero">
-			<div className="font-mono text-9xl">Hi, I'm Julia</div>
+			<div className="font-mono lg:text-9xl text-8xl">
+				Hi, <br /> I'm Julia
+			</div>
 			<div className="top-[200px] absolute h-[100px] lg:w-[900px] md:object-scale-down">
 				<BackgroundAnimation></BackgroundAnimation>
 			</div>
