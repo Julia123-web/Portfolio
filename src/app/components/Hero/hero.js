b06debe0195a800d05ba1ec1933b98cabaf5940a
Julia <juliadesouzaesilva@gmail.com>
2024-03-26 17:03:35 +0100

    typing animation added

--- src/app/components/Hero/hero.js
+++ src/app/components/Hero/hero.js
@@ -1,9 +1,41 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import BackgroundAnimation from '../backgroundAnimation/backgroundAnimation';
 
 export default function Hero() {
+	const [typedText, setTypedText] = useState('');
+	const texts = ['Hello,', 'My name is Julia'];
+
+	useEffect(() => {
+		let currentTextIndex = 0;
+		let currentText = '';
+		let intervalId;
+
+		const type = () => {
+			if (currentTextIndex === texts.length) {
+				currentTextIndex = 0;
+			}
+			const targetText = texts[currentTextIndex];
+			const typingInterval = setInterval(() => {
+				currentText = targetText.slice(0, currentText.length + 1);
+				setTypedText(currentText);
+				if (currentText === targetText) {
+					clearInterval(typingInterval);
+					setTimeout(() => {
+						currentTextIndex++;
+						type();
+					}, 1000);
+				}
+			}, 150);
+			intervalId = typingInterval;
+		};
+
+		type();
+
+		return () => clearInterval(intervalId);
+	}, []);
+
 	return (
 		<div className="flex justify-center items-center text-center text-4xl" id="hero">
 			<motion.div
@@ -16,10 +48,19 @@ export default function Hero() {
 				}}
 				className="font-mono lg:text-9xl text-8xl"
 			>
-				Hello, <br /> My name is Julia
+				{typedText && (
+					<>
+						{typedText}
+						<motion.span
+							className="inline-block h-[40px] w-[3px] bg-orange-500 ml-1"
+							animate={{ scaleY: [0, 1, 0] }}
+							transition={{ duration: 0.5, repeat: Infinity }}
+						/>
+					</>
+				)}
 			</motion.div>
 			<div className="top-[200px] absolute h-[100px] lg:w-[900px] md:object-scale-down">
-				<BackgroundAnimation></BackgroundAnimation>
+				<BackgroundAnimation />
 			</div>
 		</div>
 	);
