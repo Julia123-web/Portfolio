887902133106a7ac1c74af7a52d143ac693d30be
JuliaSilva <juliadesouzaesilva@gmail.com>
2025-03-13 11:13:12 +0100

    added new gifs in projects

--- src/app/components/Projects/projects.js
+++ src/app/components/Projects/projects.js
@@ -5,6 +5,8 @@ import myFirst from '../../../../public/first.gif';
 import mySecond from '../../../../public/second.gif';
 import myThird from '../../../../public/third.gif';
 import myFourth from '../../../../public/fourth.gif';
+import Retro from '../../../../public/retroShop.gif';
+import Ramro from '../../../../public/RamroLife.gif';
 import Image3 from 'next/image';
 
 export default function Projects() {
@@ -25,7 +27,7 @@ export default function Projects() {
         </div>
 
         {/* Projects Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 justify-items-center place-items-center max-w-[1100px] mx-auto px-4">
           {/* Project 1 */}
           <div className="relative group overflow-hidden rounded-lg">
             <Image3
@@ -80,33 +82,27 @@ export default function Projects() {
           <p>
             Using my skills in JavaScript, React, HTML, CSS and Tailwind. I've had the privilege of building custom UI templates
             that helped clients automate their video production process.
-            Working building those UI templates made me want more, that's why I decided to become a freelancer and continue my journey building nice templates for clients, also using Shopify, Wordpress, whatever the client need.
-            Below some other projects I worked on.
+            Working building those UI templates made me want more, that's why I decided to continue my journey also using Shopify, Wordpress.
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Project 1 */}
-          <div className="relative group overflow-hidden rounded-lg">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 justify-items-center place-items-center max-w-[1100px] mx-auto px-4">
+          <div className="relative group overflow-hidden rounded-lg h-[180px] md:h-[220px] lg:h-[250px] w-full md:w-[400px] lg:w-[500px] max-w-[500px]">
             <Image3
-              src={myFirst}
-              alt="first project"
-              className="transform transition duration-300 ease-in-out group-hover:scale-105"
+              src={Retro}
+              alt="retro project"
+              className="transform transition duration-300 ease-in-out group-hover:scale-105 h-full object-contain md:object-cover"
             />
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
-            <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            </div>
+            <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
           </div>
-
-          {/* Project 2 */}
-          <div className="relative group overflow-hidden rounded-lg">
+          <div className="relative group overflow-hidden rounded-lg h-[180px] md:h-[220px] lg:h-[250px] w-full md:w-[400px] lg:w-[500px] max-w-[500px]">
             <Image3
-              src={mySecond}
-              alt="second project"
-              className="transform transition duration-300 ease-in-out group-hover:scale-105"
+              src={Ramro}
+              alt="Ramro project"
+              className="transform transition duration-300 ease-in-out group-hover:scale-105 h-full object-contain md:object-cover"
             />
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
-            <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            </div>
+            <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
           </div>
         </div>
       </div>
